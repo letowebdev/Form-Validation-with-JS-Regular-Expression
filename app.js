3fdafaa83f7d8afc30168106b9fba4a14ce1f520
Abdelatif Zache <zacheabdelatif@outlook.de>
2020-05-15 05:14:00 +0100

    RE methods

--- app.js
+++ app.js
@@ -37,11 +37,28 @@ function validateZipCode()
 //Email Function
 function validateEmail()
 {
-    
+    const email = document.getElementById('email');
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zAZ]{2,6})$/;
+
+    if (!re.test(email.value)) 
+    {
+        email.classList.add('is-invalid');
+    } else {
+        email.classList.remove('is-invalid');
+    }
 }
 
 //Phone number Function
 function validatePhoneNumber()
 {
-    
+    const phone = document.getElementById('phone');
+    // [+][Area-Code][Phone Number] 
+    const re = /^\(?\+?\(?\d{1,3}\)?[-. ]?\d{2,3}[-. ]?\d{4,8}$/;
+
+    if (!re.test(phone.value)) 
+    {
+        phone.classList.add('is-invalid');
+    } else {
+        phone.classList.remove('is-invalid');
+    }
 }
